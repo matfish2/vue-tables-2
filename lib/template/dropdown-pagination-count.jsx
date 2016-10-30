2c5d3b022d75a34b11cd8980a1ff964ae0e4ac76
Matanya Fishaimer <matfish2@gmail.com>
2016-10-30 21:23:32 +0200

    handle dropdown pagination count text

--- lib/template/dropdown-pagination-count.jsx
+++ lib/template/dropdown-pagination-count.jsx
@@ -1,4 +1,5 @@
 module.exports = function(h, that) {
+
   if (that.count>0 && that.opts.pagination.dropdown) {
 
   let perPage = parseInt(that.limit);
@@ -6,8 +7,15 @@ module.exports = function(h, that) {
    let from = ((that.Page-1) * perPage) + 1;
    let to = that.Page==that.totalPages?that.count:from + perPage - 1;
 
+    let parts = that.opts.texts.count.split('|');
+    let i = Math.min(that.count==1?2:that.totalPages==1?1:0, parts.length-1);
+
+    let count =  parts[i].replace('{count}', that.count)
+                   .replace('{from}', from)
+                   .replace('{to}', to)
+
    return <div class="VuePagination">
- <p class="VuePagination__count">{that.display('count', {count:that.count, from, to})}</p>
+ <p class="VuePagination__count">{count}</p>
  </div>
 }
 
