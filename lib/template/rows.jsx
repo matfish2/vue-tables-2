afa75875f2daab243d34df4729c622577b74df0d
Matanya Fishaimer <matfish2@gmail.com>
2016-12-06 21:03:29 +0200

    add childRowKey

--- lib/template/rows.jsx
+++ lib/template/rows.jsx
@@ -1,6 +1,7 @@
 module.exports = function(h, that) {
     var rows = [];
     var columns;
+    var rowKey = that.opts.childRowKey;
 
     var data = that.source=='client'?that.filteredData:that.tableData;
 
@@ -8,7 +9,7 @@ module.exports = function(h, that) {
       columns = [];
 
       if (that.opts.childRow)
-       columns.push(<td><span on-click={that.toggleChildRow.bind(that,row.id)} class={`VueTables__child-row-toggler ` + that.childRowTogglerClass(row.id)}></span></td>);
+       columns.push(<td><span on-click={that.toggleChildRow.bind(that,row[rowKey])} class={`VueTables__child-row-toggler ` + that.childRowTogglerClass(row[rowKey])}></span></td>);
 
       that.allColumns.map(function(column) {
           columns.push(<td>
@@ -19,7 +20,7 @@ module.exports = function(h, that) {
       rows.push(<tr on-click={that.rowWasClicked.bind(that, row)}>{columns} </tr>);
 
       if (that.opts.childRow)
-        rows.push(<tr class={`VueTables__child-row ` + that.childRowClass(row.id)}><td colspan={that.allColumns.length+1}>{that.opts.childRow.apply(that, [h, row])}</td></tr>);
+        rows.push(<tr class={`VueTables__child-row ` + that.childRowClass(row[rowKey])}><td colspan={that.allColumns.length+1}>{that.opts.childRow.apply(that, [h, row])}</td></tr>);
 
     }.bind(that))
 
