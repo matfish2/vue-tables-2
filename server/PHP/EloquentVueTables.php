73d5eef44a5885f951e52fa74499542a27cc1b25
Saiful Islam <saaifulislam@gmail.com>
2017-05-29 14:14:41 +0600

    Update EloquentVueTables.php
    If $with empty handled

--- server/PHP/EloquentVueTables.php
+++ server/PHP/EloquentVueTables.php
@@ -11,7 +11,7 @@ Class EloquentVueTables  implements VueTablesInterface  {
 
     extract(Input::only('query', 'limit', 'page', 'orderBy', 'ascending', 'byColumn'));
 
-    $data = $model->select($fields)->with($with);
+    $data = (empty($with))? $model->select($fields) : $model->select($fields)->with($with);
 
     if (isset($query) && $query) {
        $data = $byColumn==1?$this->filterByColumn($data, $query):
