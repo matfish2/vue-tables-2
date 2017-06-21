1d27be5ad255de8b64a7ae147f3520b50714365f
Matanya Fishaimer <matfish2@gmail.com>
2017-06-22 00:33:09 +0300

    update version after PR

--- compiled/template/normal-filter.js
+++ compiled/template/normal-filter.js
@@ -2,38 +2,38 @@
 
 module.exports = function (h, that) {
 
-    if (!that.opts.filterable) return '';
+  if (!that.opts.filterable) return '';
 
-    var search = that.source == 'client' ? that.search.bind(that, that.data) : that.serverSearch.bind(that);
+  var search = that.source == 'client' ? that.search.bind(that, that.data) : that.serverSearch.bind(that);
 
-    if (that.opts.filterable && !that.opts.filterByColumn) {
-        var id = 'VueTables__search_' + that.id;
-        return h(
-            'div',
-            { 'class': 'form-group form-inline pull-left VueTables__search' },
-            [h(
-                'label',
-                {
-                    attrs: { 'for': id }
-                },
-                [that.display('filter')]
-            ), h(
-                'input',
-                { 'class': 'form-control',
-                    attrs: { type: 'text',
-                        value: that.query,
-                        placeholder: that.display('filterPlaceholder'),
+  if (that.opts.filterable && !that.opts.filterByColumn) {
+    var id = 'VueTables__search_' + that.id;
+    return h(
+      'div',
+      { 'class': 'form-group form-inline pull-left VueTables__search' },
+      [h(
+        'label',
+        {
+          attrs: { 'for': id }
+        },
+        [that.display('filter')]
+      ), h(
+        'input',
+        { 'class': 'form-control',
+          attrs: { type: 'text',
+            value: that.query,
+            placeholder: that.display('filterPlaceholder'),
 
-                        id: id
-                    },
-                    on: {
-                        'keyup': search
-                    }
-                },
-                []
-            )]
-        );
-    }
+            id: id
+          },
+          on: {
+            keyup: search
+          }
+        },
+        []
+      )]
+    );
+  }
 
-    return '';
+  return '';
 };
