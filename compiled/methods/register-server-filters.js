35330f4280dacb6e1d3575e9de1d5be0562dd963
Matanya Fishaimer <matfish2@gmail.com>
2017-07-04 19:44:38 +0300

    fix #205

--- compiled/methods/register-server-filters.js
+++ compiled/methods/register-server-filters.js
@@ -8,9 +8,12 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 module.exports = function () {
 
+  var event = 'vue-tables';
+  if (this.name) event += '.' + this.name;
+
   this.opts.customFilters.forEach(function (filter) {
-    _bus2.default.$off('vue-tables.filter::' + filter);
-    _bus2.default.$on('vue-tables.filter::' + filter, function (value) {
+    _bus2.default.$off(event + '.filter::' + filter);
+    _bus2.default.$on(event + '.filter::' + filter, function (value) {
       this.customQueries[filter] = value;
       this.updateState('customQueries', this.customQueries);
       this.refresh();
