12757a6f34a5948a768c393ea0fc3161b46ed22a
Matanya Fishaimer <matfish2@gmail.com>
2017-07-29 15:10:56 +0300

    emit events even when using vuex (#228)

--- compiled/methods/dispatch.js
+++ compiled/methods/dispatch.js
@@ -7,16 +7,16 @@ var _bus2 = _interopRequireDefault(_bus);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = function (event, payload) {
+
   if (this.vuex) {
     if (event.split('::').length > 1) return;
     this.commit(event.toUpperCase().replace('-', '_'), payload);
-  } else {
-
-    var name = 'vue-tables.';
-    if (this.name) name += this.name + ".";
-    name += event;
-
-    _bus2.default.$emit(name, payload);
-    this.$emit(event, payload);
   }
+
+  var name = 'vue-tables.';
+  if (this.name) name += this.name + ".";
+  name += event;
+
+  _bus2.default.$emit(name, payload);
+  this.$emit(event, payload);
 };
