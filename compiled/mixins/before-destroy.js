c93c6a40a6ceadb9f0e071614b5a31400d2501d6
Matanya <matfish2@gmail.com>
2017-12-04 18:24:18 +0200

    remove event buses before destroy (#347)

---
+++ compiled/mixins/before-destroy.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _bus = require('../bus');
+
+var _bus2 = _interopRequireDefault(_bus);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = function () {
+    _bus2.default.$off();
+    _bus2.default.$destroy();
+};
--- compiled/table.js
+++ compiled/table.js
@@ -5,9 +5,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 exports.default = function () {
-  return { methods: methods, computed: computed, directives: directives };
+  return { methods: methods, computed: computed, directives: directives, beforeDestroy: beforeDestroy };
 };
 
 var methods = require('./mixins/methods');
 var computed = require('./mixins/computed');
 var directives = require('./mixins/directives');
+var beforeDestroy = require('./mixins/before-destroy');
