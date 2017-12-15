7e9b65af1aea2c37b17636e078a903490650e953
Matanya <matfish2@gmail.com>
2017-12-15 08:17:11 +0200

    add columns display tests

---
+++ test/columns-display.spec.js
@@ -0,0 +1,81 @@
+describe(suite + ': Columns Display', () => {
+
+    var dropdown = '.VueTables__columns-dropdown';
+    
+    beforeEach(()=>{
+        setOptions({
+            columnsDropdown: true
+        });
+    });
+
+    it.only('can optionally add a columns display dropdown', () => {
+
+        setOptions({
+            columnsDropdown: false
+        });
+
+        not_exists(dropdown);
+
+        setOptions({
+            columnsDropdown: true
+        });
+
+        exists(dropdown);
+    });
+
+    it.only('Toggles the dropdown', ()=>{
+
+        expect(vm().displayColumnsDropdown).toBe(false);
+
+        click('.dropdown-toggle');
+
+        expect(vm().displayColumnsDropdown).toBe(true);
+        
+    });
+
+    it.only('Checks all columns by default', ()=>{
+        var els = getCheckboxes();
+
+        els.forEach(el => {
+            expect(el.element.checked).toBe(true);
+        });
+
+    });
+
+    it.only('can toggle columns', ()=>{
+        var els = getCheckboxes();
+
+        see('Code','table thead tr:first-child th:first-child');
+        count('table thead tr:first-child th',3);
+        
+        toggleColumn(1);
+
+        var first = els.shift();
+
+        expect(first.element.checked).toBe(false);
+
+        els.forEach(el => {
+            expect(el.element.checked).toBe(true);
+        });
+
+        not_see('Code','table thead tr:first-child th:first-child');
+        see('Name','table thead tr:first-child th:first-child');
+        count('table thead tr:first-child th',2);
+
+        toggleColumn(1);
+
+        see('Code','table thead tr:first-child th:first-child');
+        count('table thead tr:first-child th',3);
+        
+    });
+
+
+});
+
+function getCheckboxes() {
+    return wrapper.find('ul.dropdown-menu').findAll('input').wrappers;
+}
+
+function toggleColumn(num) {
+    click(`ul.dropdown-menu li:nth-child(${num}) input`);    
+}
