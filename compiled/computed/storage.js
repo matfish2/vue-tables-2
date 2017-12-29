70b747d00fde2fb8f7c053546bc54d6f7d6bb775
Matanya <matfish2@gmail.com>
2017-12-29 23:39:20 +0200

    check localstorage existance

--- compiled/computed/storage.js
+++ compiled/computed/storage.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 module.exports = function () {
-  return this.opts.storage === 'local' ? localStorage : sessionStorage;
+  return this.opts.storage === 'local' && (typeof localStorage === 'undefined' ? 'undefined' : _typeof(localStorage)) !== undefined ? localStorage : sessionStorage;
 };
