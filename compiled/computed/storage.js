06c0af9fe93449d189839fb402ac42007977fc2b
Matanya <matfish2@gmail.com>
2017-12-29 23:52:27 +0200

    update vue-test-utils version

--- compiled/computed/storage.js
+++ compiled/computed/storage.js
@@ -1,10 +1,8 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 module.exports = function () {
 
-  if ((typeof localStorage === 'undefined' ? 'undefined' : _typeof(localStorage)) === undefined) return {};
+  if (typeof localStorage === 'undefined') return {};
 
   return this.opts.storage === 'local' ? localStorage : sessionStorage;
 };
