2fa9fa8822275cdd69491509abe8c807f1f3f6e1
Matanya <matfish2@gmail.com>
2018-02-09 17:56:21 +0200

    fix #420

--- compiled/mixins/before-destroy.js
+++ compiled/mixins/before-destroy.js
@@ -1,16 +1,25 @@
-'use strict';
+"use strict";
 
-var _bus = require('../bus');
+var _bus = require("../bus");
 
 var _bus2 = _interopRequireDefault(_bus);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = function () {
+    var _this = this;
+
+    var el;
+
     _bus2.default.$off();
     _bus2.default.$destroy();
 
     if (this.vuex && !this.opts.preserveState) {
         this.$store.unregisterModule(this.name);
     }
+
+    this.opts.dateColumns.forEach(function (column) {
+        el = $(_this.$el).find("#VueTables__" + column + "-filter");
+        el.data('daterangepicker').remove();
+    });
 };
