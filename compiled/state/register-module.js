496347579e175b1a991fffb9fcb661c923c63860
matfish2 <matfish2@gmail.com>
2018-06-22 21:08:53 +0100

    resolve #407

--- compiled/state/register-module.js
+++ compiled/state/register-module.js
@@ -8,23 +8,24 @@ var _mutations = require('./mutations');
 
 var _mutations2 = _interopRequireDefault(_mutations);
 
+var _merge = require('merge');
+
+var _merge2 = _interopRequireDefault(_merge);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = function (self) {
 
-  var initialState;
-
-  if (self.$store && self.$store.state && self.$store.state[self.name]) {
-    initialState = self.$store.state[self.name];
-    self.$store.unregisterModule(self.name);
-  } else {
-    initialState = (0, _state2.default)(self);
-  }
-
-  var store = {
-    state: initialState,
+  var Module = {
+    state: (0, _state2.default)(self),
     mutations: (0, _mutations2.default)(self)
   };
 
-  self.$store.registerModule(self.name, store);
+  if (self.$store && self.$store.state && self.$store.state[self.name]) {
+    console.log("unregister module");
+    Module.state = _merge2.default.recursive(Module.state, self.$store.state[self.name]);
+    self.$store.unregisterModule(self.name);
+  }
+
+  self.$store.registerModule(self.name, Module);
 };
