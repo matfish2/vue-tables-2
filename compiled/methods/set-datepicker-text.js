d77183c305640093e10a0d37affe5636d31c2c14
Brian Crist <Brian.Crist@calcartage.com>
2018-11-14 16:17:11 -0800

    adds escapeSelectors to remaining daterangepicker selectors

--- compiled/methods/set-datepicker-text.js
+++ compiled/methods/set-datepicker-text.js
@@ -3,7 +3,7 @@
 module.exports = function (column, start, end) {
 
     var dateFormat = this.dateFormat(column);
-    var el = typeof column === 'string' ? $(this.$el).find("#VueTables__" + column + "-filter") : column;
+    var el = typeof column === 'string' ? $(this.$el).find("#VueTables__" + $.escapeSelector(column) + "-filter") : column;
 
     el.text(start.format(dateFormat) + " - " + end.format(dateFormat));
 };
