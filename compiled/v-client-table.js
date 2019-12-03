60208b720956aee13e0aca0c59ea1af2074d1722
matfish2 <matfish2@gmail.com>
2019-12-03 19:53:27 +0200

    add resizable columns whitelist option

--- compiled/v-client-table.js
+++ compiled/v-client-table.js
@@ -68,7 +68,7 @@ exports.install = function (Vue, globalOptions, useVuex) {
       this._setFiltersDOM(this.query);
 
       if (this.opts.resizableColumns) {
-        (0, _resizeableColumns["default"])(this.$el.querySelector("table"), this.hasChildRow, this.opts.childRowTogglerFirst);
+        (0, _resizeableColumns["default"])(this.$el.querySelector("table"), this.hasChildRow, this.opts.childRowTogglerFirst, this.opts.resizableColumns);
       }
 
       this._setColumnsDropdownCloseListener();
