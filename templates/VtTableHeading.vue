46e4da009add5d3631cf6741d1ada6011619e118
Matanya Fishaimer <matfish2@gmail.com>
2020-05-07 22:11:49 +0300

    fix table heading template (#830)

--- templates/VtTableHeading.vue
+++ templates/VtTableHeading.vue
@@ -3,8 +3,10 @@
         v-on="props.thEvents"
         v-bind="props.thAttrs"
     >
-        <span class="VueTables__heading" :title="props.title">{{props.heading}}</span>
-       <vt-sort-control/>
+        <span class="VueTables__heading" :title="props.title">
+            <vnodes :vnodes="props.heading"/>
+        </span>
+        <vt-sort-control/>
     </th>
 </template>
 
@@ -12,8 +14,14 @@
     import VtSortControl from "vue-tables-2/compiled/components/VtSortControl";
 
     export default {
-        name: "MyTableHeading",
-        components:{ VtSortControl },
+        name: "VtTableHeading",
+        components: {
+            VtSortControl,
+            vnodes: {
+                functional: true,
+                render: (h, ctx) => typeof ctx.props.vnodes === 'object' ? ctx.props.vnodes : [ctx.props.vnodes]
+            }
+        },
         props: ['props']
     }
 </script>
