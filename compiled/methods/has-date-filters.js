1941fdeabab7e84a9ce12c29c3e5e969f98230c5
Matanya Fishaimer <matfish2@gmail.com>
2020-05-16 00:54:21 +0300

    fix

--- compiled/methods/has-date-filters.js
+++ compiled/methods/has-date-filters.js
@@ -2,7 +2,7 @@
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-var intersection = require('array-intersect');
+var intersection = require('array-intersect')["default"];
 
 module.exports = function () {
   var opts = this.opts;
